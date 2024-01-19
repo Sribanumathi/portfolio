f85d1b1186aed780bc6529b5c0af54741dee91df
unknown <soficivil1998@gmail.com>
2024-01-19 16:57:09 +0530

    first time push

---
+++ my-portfolio/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MaincontentComponent } from './maincontent/maincontent.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet,HeaderComponent,FooterComponent,MaincontentComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'my-portfolio';
+}
